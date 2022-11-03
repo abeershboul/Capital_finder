a7374c865e40490c3ad83c230a9bb09adc674e57
abeershboul <abeer.shboul@yahoo.com>
2022-11-03 12:03:38 +0200

    complete project

---
+++ api/capital-finder.py
@@ -0,0 +1,40 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+class handler(BaseHTTPRequestHandler):
+     def do_GET(self):
+        url_path = self.path
+        url_components = parse.urlsplit(url_path)
+        query_list = parse.parse_qsl(url_components.query)
+        dictionary  = dict(query_list)
+
+
+        if 'country' in dictionary :
+            country = dictionary ['country']
+            url = 'https://restcountries.com/v3.1/name/'
+            response  = requests.get(url + country)
+            data = response.json()
+            capital = data[0]['capital'][0]
+            resalts = f"The capital of {dictionary ['country']} is {capital}"
+
+        elif 'capital' in dictionary :
+            capital = dictionary ['capital']
+            url ='https://restcountries.com/v3.1/capital/'
+            response  = requests.get(url+capital)
+            data = response.json()
+            country = data[0]['name']['common']
+            resalts = f"The {dictionary ['capital']} is capital of {country}"
+            
+
+        else:
+            resalts = "Please provide me with a correct name"
+       
+
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(resalts.encode())
+
+        return
