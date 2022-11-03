e3fab6d74a41ce640e3e54006bafc400aef78f3f
abeershboul <abeer.shboul@yahoo.com>
2022-11-03 14:15:26 +0200

    new

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -3,38 +3,29 @@ from urllib import parse
 import requests
 
 class handler(BaseHTTPRequestHandler):
-     def do_GET(self):
-        url_path = self.path
-        url_components = parse.urlsplit(url_path)
-        query_list = parse.parse_qsl(url_components.query)
-        dictionary  = dict(query_list)
 
+  def do_GET(self):
+    '''A function that send request to vercel'''
+    path = self.path
+    url_components = parse.urlsplit(path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_string_list)
 
-        if 'country' in dictionary :
-            country = dictionary['country']
-            url = 'https://restcountries.com/v3.1/name/'
-            response  = requests.get(url + country)
-            data = response.json()
-            capital = data[0]['capital'][0]
-            resalts = f"The capital of {dictionary ['country']} is {capital}"
+    if "country" in dic:
+      url = "https://restcountries.com/v3.1/name/"
+      r = requests.get(url + dic['country'])
+      data = r.json()
+      message = f"The Capital of {dic['country']} is {data[0]['capital'][0]}"
+    elif "capital" in dic:
+      url = "https://restcountries.com/v3.1/capital/"
+      r = requests.get(url + dic['capital'])
+      data = r.json()
+      message = f"{dic['capital']} is the Capital of {data[0]['name']['common']}"
+    else:
+      message = "Define a country or capital please"
 
-        elif 'capital' in dictionary :
-            capital = dictionary['capital']
-            url ='https://restcountries.com/v3.1/capital/'
-            response  = requests.get(url+capital)
-            data = response.json()
-            country = data[0]['name']['common']
-            resalts = f"The {dictionary['capital']} is capital of {country}"
-            
-
-        else:
-            resalts = "Please provide me with a correct name"
-       
-
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(resalts.encode())
-
-        return
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(message.encode())
+    return
